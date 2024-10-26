9097f0b92847f1cbed47174c70860025c1e8e8fb
Amar Prakash <prakashamar730@gmail.com>
2024-10-26 09:10:10 +0530

    update file

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-// console.log("Hello World!");
-// let a;
-// console.log(a);
+function min(...args) {
+    let min = args.reduce((a ,e) => {
+        if(a > e) return e;
+        else return a;
+    });
+    return min;
+}
+
+console.log(min(1, 2, 3, 4, 5, 6, 7));
