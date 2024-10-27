b606b54e61c9a2844e4c7acd84e399662d2fe0b1
Amar Prakash <prakashamar730@gmail.com>
2024-10-27 12:26:36 +0530

    update file

--- script.js
+++ script.js
@@ -1,37 +1,72 @@
-function min(...args) {
-    let min = args.reduce((a ,e) => {
-        if(a > e) return e;
-        else return a;
-    });
-    return min;
-}
+// function min(...args) {
+//     let min = args.reduce((a ,e) => {
+//         if(a > e) return e;
+//         else return a;
+//     });
+//     return min;
+// }
 
 
-let arr = [-2, 4, 2, -5, 8, 6, -9];
+// let arr = [-2, 4, 2, -5, 8, 6, -9];
 
-let [x, y, ...others] = arr;
+// let [x, y, ...others] = arr;
 
-let obj = {
-    name: "Amar Prkash",
-    age: 21,
-    city: "Bhopal",
-}
+// let obj = {
+//     name: "Amar Prkash",
+//     age: 21,
+//     city: "Bhopal",
+// }
 
-let {name: naam, age, isMarried = false} = obj;
+// let {name: naam, age, isMarried = false} = obj;
 
 
-let obj2 = {
-    func: function() {
-        console.log(this);  
-    },
-    func2: () => {
-        console.log(this);
-    }
-}
+// let obj2 = {
+//     func: function() {
+//         console.log(this);  
+//     },
+//     func2: () => {
+//         console.log(this);
+//     }
+// }
 
-let brr = [2, 4, 6];
+// let brr = [2, 4, 6];
 
-let isAllEven = brr.every(ele => ele%2==0);
+// let isAllEven = brr.every(ele => ele%2==0);
+
+// let btn = document.querySelector("button");
+// let h1 = document.querySelector("h1");
+// let div = document.querySelector("#box");
+
+// btn.addEventListener("click", () => {
+
+//     let r1 = Math.floor(Math.random()*256);
+//     let r2 = Math.floor(Math.random()*256);
+//     let r3 = Math.floor(Math.random()*256);
+
+//     h1.innerText = `rgb(${r1}, ${r2}, ${r3})`;
+//     div.style.backgroundColor = `rgb(${r1}, ${r2}, ${r3})`;
+
+// });
+
+// let inp = document.querySelector("input");
+// let div = document.querySelector("div");
+
+// div.addEventListener("keydown", function(event) {
+//     console.log("KeyBoard event triggered!");
+// });
+
+// inp.addEventListener("keydown", function(event) {
+//     console.log("KeyBoard event triggered!");
+// });
+
+
+
+
+
+
+
+
+
 
 
 
