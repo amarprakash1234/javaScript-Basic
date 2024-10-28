8de13381ecb9e77b23bf62d65861d0f709a95822
Amar Prakash <prakashamar730@gmail.com>
2024-10-28 15:48:09 +0530

    Update file

--- script.js
+++ script.js
@@ -59,6 +59,98 @@
 //     console.log("KeyBoard event triggered!");
 // });
 
+// By Code Help
+
+//code 1 -> slow
+// const t1 = performance.now();
+
+// for(let i = 1; i <= 100; i++) {
+//     let para = document.createElement("p");
+//     para.textContent = "This is para " + i;
+//     document.body.appendChild(para);
+// }
+
+// const t2 = performance.now();
+
+// console.log("Total time taken by code 1 : " + (t2-t1));
+
+// //code 2 -> fast
+// const t3 = performance.now();
+
+// let myDiv = document.createElement("div");
+
+// for(let i = 1; i <= 100; i++) {
+//     let para = document.createElement("p");
+//     para.textContent = "This is para " + i;
+//     myDiv.appendChild(para);
+// }
+
+// document.body.appendChild(myDiv);
+
+// const t4 = performance.now();
+
+// console.log("Total time taken by code 2 : " + (t4-t3));
+
+
+// best code
+
+// let fragment = document.createDocumentFragment();
+
+// for(let i = 1; i <= 100; i++) {
+//     let para = document.createElement("p");
+//     para.textContent = "This is para " + i;
+//     // node reflow and repaint for below line. so thats why using documentFragement is considered a good practice.
+//     fragment.appendChild(para);
+// }
+
+// the below line take 1 reflow and 1 repaint.
+// document.body.appendChild(fragment);
+
+
+// setTimeout(function sayHello() {
+//     console.log("Hello Amar Prakash!");
+
+// }, 3000);
+
+// promise!
+
+// let promise = new Promise((resolve, reject) => {
+//     console.log("Amar Prakash!");
+//     reject(101);
+// });
+
+// promise.then((msz) => {
+//     console.log("Rishikest kumar!", msz);
+//     return 102;
+// }).then((msz) => {
+//     console.log("Ved Prakash!", msz)
+//     return 103;
+// }).then((msz) => {
+//     console.log("Suraj Kumar!", msz);
+// }).catch((err) => {
+//     console.log(err);
+// }).finally((msz) => {
+//     console.log("I am final, Hamesa chalunga!");
+// })
+
+// let promise1 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 1000, "First");
+// })
+
+// let promise2 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 2000, "Second");
+// })
+
+// let promise3 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 4000, "Third");
+// })
+
+// Promise.all([promise1, promise2, promise3])
+// .then((values) => {
+//     console.log(values);
+// }).catch((err) => {
+//     console.log(err);
+// })
 
 
 
